5ab940feeecbfb268fb7bca22b7a9466e2711517
echeadle <echeadle@gmail.com>
2023-12-26 16:54:02 -0700

    Finished buildings

--- js/world.js
+++ js/world.js
@@ -80,6 +80,16 @@ class World {
             bases.push(new Envelope(seg, this.buildingWidth).poly);
         }
 
+        for (let i = 0; i < bases.length - 1; i++) {
+            for (let j = i + 1; j < bases.length; j++) {
+                if (bases[i].intersectsPoly(bases[j])) {
+                    bases.splice(j, 1);
+                    j--;
+                }
+
+            }
+        }
+
         return bases;
     }
 
