a955568fa04cb37ab3717c2e49b9b1396de125af
echeadle <echeadle@gmail.com>
2023-12-27 16:59:47 -0700

    Add stick to represent tree trunk

--- js/items/tree.js
+++ js/items/tree.js
@@ -6,5 +6,8 @@ class Tree {
 
     draw(ctx) {
         this.center.draw(ctx, { size: this.size, color: "green" });
+
+        const top = add(this.center, {x: -40, y: -40 });
+        new Segment(this.center, top).draw(ctx)
     }
 }
