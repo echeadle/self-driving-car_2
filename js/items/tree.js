895f93501537995d176b5147af79b77f1c043f29
echeadle <echeadle@gmail.com>
2023-12-27 19:00:20 -0700

    Finish trees

--- js/items/tree.js
+++ js/items/tree.js
@@ -3,6 +3,18 @@ class Tree {
         this.center = center;
         this.size = size; // size of the base
         this.heightCoef = heightCoef;
+        this.base = this.#generateLevel(center, size);
+    }
+
+    #generateLevel(point, size) {
+        const points = []
+        const rad = size / 2;
+        for (let a = 0; a < Math.PI * 2; a += Math.PI / 16) {
+            const kindOfRandom = Math.cos(((a + this.center.x) * size) % 17) ** 2; 
+            const noisyRadius = rad * lerp(0.5, 1, kindOfRandom);
+            points.push(translate(point, a, noisyRadius));
+        } 
+        return new Polygon(points);
     }
 
     draw(ctx, viewPoint) {
@@ -15,7 +27,9 @@ class Tree {
             const t = level / (levelCount - 1);
             const point = lerp2D(this.center, top, t)
             const color = "rgb(30," + lerp(50, 200, t) + ",70)";
-            point.draw(ctx, { size: this.size, color: color });
+            const size = lerp(this.size, 40, t);
+            const poly = this.#generateLevel(point, size);
+            poly.draw(ctx, { fill: color, stroke: "rgba(0,0,0.0)" });
         }
     }
 }
