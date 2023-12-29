715f1079f14238930476e2d5c8afcda9752003cf
echeadle <echeadle@gmail.com>
2023-12-29 09:31:46 -0700

    Finish crosswalks first pass

--- js/markings/crossing.js
+++ js/markings/crossing.js
@@ -1,4 +1,4 @@
-class Stop {
+class Crossing {
     constructor(center, directionVector, width, height) {
         this.center = center;
         this.directionVector = directionVector;
@@ -16,22 +16,16 @@ class Stop {
     }
 
     draw(ctx) {
-        this.border.draw(ctx, { width: 5, color: "white"});
-        ctx.save();
-        ctx.translate(this.center.x, this.center.y);
-        ctx.rotate(angle(this.directionVector) - Math.PI /2);
-        ctx.scale(1, 3);
-
-
-        ctx.beginPath();
-        this.poly.draw(ctx);
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "center";
-        ctx.fillStyle = "white";
-        ctx.font = "bold" + this.height * 0.3 + "px Arial";
-        ctx.fillText("STOP", 0, 0);
-
-        ctx.restore();
+        const perp = perpendicular(this.directionVector);
+        const line = new Segment(
+            add(this.center, scale(perp, this.width / 2)),
+            add(this.center, scale(perp, -this.width / 2))
+        );
+        line.draw(ctx, {
+            width: this.height,
+            color: "white",
+            dash: [11, 11]
+        });
     }
 
 }
